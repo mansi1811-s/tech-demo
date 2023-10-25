8e7c7e5df6e65ec6d1aa1cc436c06bc12732ca34
Mansi Singh <72257441+mansi1811-s@users.noreply.github.com>
2023-10-25 13:39:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,18 @@
-# from flask import Flask
-# # from decouple import config 
-# app = Flask(__name__)
+from flask import Flask
 
-# # #passing password as environment variable
-# # password = config('PASSWORD')
+app = Flask(__name__)
 
-# #password in plain text
-# password = "xyz"
+#password in plain text
+password = "xyz"
 
-# @app.route("/")
-# def hello():
-#     print(password)
-#     return "Hello, This is CodeQL demo!"
+@app.route("/")
+def hello():
+    print(password)
+    return "Hello, This is CodeQL demo!"
 
-# if __name__ == "__main__":
-#     app.run()
+if __name__ == "__main__":
+    app.run()
 
 
-# import mysql.connector
-
-# with mysql.connector.connect(user='user', password='password', host='host', database='database') as conn:
-#     cursor = conn.cursor()
-    
-#     name = "John"
-#     query = "SELECT * FROM users WHERE name = %s"
-#     cursor.execute(query, (name,))
-#     rows = cursor.fetchall()
 
 
-import sqlite3
-
-def get_user_data(username):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
-    #Correct code without vulnerability
-    # query = "SELECT * FROM users WHERE username = ?"
-
-    #Directly concatenating user input into SQL query -CVE-2018-6829
-    query = "SELECT * FROM users WHERE username = '" + username + "'"
-    cursor.execute(query)
-
-    user_data = cursor.fetchall()
-    conn.close()
-    return user_data
-
-username = input("Enter a username: ")
-user_data = get_user_data(username)
-print(user_data)
