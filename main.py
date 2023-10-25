b00a173f149158a074d663a4d56e8356664fbf29
Mansi Singh <72257441+mansi1811-s@users.noreply.github.com>
2023-10-25 09:07:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from flask import Flask
-from decouple import config 
-app = Flask(__name__)
+# from flask import Flask
+# from decouple import config 
+# app = Flask(__name__)
 
-#passing password as environment variable
-password = config('PASSWORD')
+# #passing password as environment variable
+# password = config('PASSWORD')
 
-# #password in plain text
-# password = "xyz"
+# # #password in plain text
+# # password = "xyz"
 
-@app.route("/")
-def hello():
-    print(password)
-    return "Hello, This is CodeQL demo!"
+# @app.route("/")
+# def hello():
+#     print(password)
+#     return "Hello, This is CodeQL demo!"
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
+
+import mysql.connector
+
+with mysql.connector.connect(user='user', password='password', host='host', database='database') as conn:
+    cursor = conn.cursor()
+    
+    name = "John"
+    query = "SELECT * FROM users WHERE name = %s"
+    cursor.execute(query, (name,))
+    rows = cursor.fetchall()
